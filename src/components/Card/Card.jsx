85a58d1d968c7808a3d36eb4769cb6ee954e7ff3
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-01 21:17:57 -0300

    Mi App Rick and Morty

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card({name, status, species, gender, origin, image, onclose }) {
+   return (
+      <div>
+         <button onClick={onclose}>X</button>
+         <h2>{name}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin}</h2>
+         <img src={image} alt='Rick Sanchez' />
+      </div>
+   );
+}
