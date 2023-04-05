97006284983dbce4e5035d74966bbe11dbe570a3
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-04 22:40:09 -0300

    4/4

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Detail from './components/Detail/Detail.jsx';
  
 function App() {
    const [characters, setCharacters] = useState([]);
+   const [showImage, setShowImage] = useState(true);
    
    
    const onSearch = (id) => {
@@ -18,14 +19,18 @@ function App() {
       .then((data) => {
          if (data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
+            setShowImage(false);
          } else {
             alert('¡No hay personajes con este ID!');
          }
       });
-   }     
+   }    
    const onClose = (id) => {
       const charactersFiltered = characters.filter(character => character.id !== Number(id))
       setCharacters(charactersFiltered)
+      if (charactersFiltered.length === 0) {
+         setShowImage(true);
+      }
    }
 
    // const getRandomCharacter = () => {
@@ -38,9 +43,15 @@ function App() {
       <div className='App'>
          
          <Nav onSearch={onSearch} />
+
          <div className='title-image'>
          <img src='https://occ-0-5428-1740.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABWT5HIl9YXE1ZG5Khq2rGPAsxwcnKPhqJMO3E2WiZBVNemNHAlH148400SKvcFoxJFZsxLBpOCb31CliGnE3RYbxVAyHf10wyEfqZHliqF0z.png?r=a6e' />
          </div>
+         {showImage && (
+            <div className='title-image'>
+               <img src='https://es.rollingstone.com/wp-content/uploads/2022/09/La-serie-Rick-y-Morty-podria-ser-eterna.jpg' />
+            </div>
+         )}
          <Routes>
             <Route path='/home' element={<Cards onClose={onClose} characters={characters} />} />
             <Route path='/about' element={<About/>} />
