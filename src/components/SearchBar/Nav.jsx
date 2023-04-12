71621c04e4fead8f937bcae30b5d7e412abbdbe0
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-12 09:06:45 -0300

    12/4/23

--- src/components/SearchBar/Nav.jsx
+++ src/components/SearchBar/Nav.jsx
@@ -15,20 +15,33 @@ const Nav = ({onSearch, setAccess}) => {
           <NavLink to="/home" className={styles.Navboton}>HOME</NavLink>
           <NavLink to="/favorites"  className={styles.Navboton}>FAVORITES</NavLink>
           <NavLink to="/about" className={styles.Navboton}>ABOUT</NavLink>
-           
-            
+                     
             <button className={styles.Navboton} onClick={handleLogOut}>LOGOUT</button>
-            
-            
+                      
             {/* <button className={styles.Navboton} onClick={getRandomCharacter}>RANDOM</button> */}
           </div>
           <div className={styles.searchBarContainer}>
             <SearchBar onSearch={onSearch} />
             {/* <Nav onSearch={onSearch} getRandomCharacter={getRandomCharacter} /> */}
           </div>
-          
+          <nav className={styles.navbar}>
+         <div className={styles.container}>
+          <div className={styles.navelements}>
+            <ul>
+              <li ><NavLink to="/home" >HOME</NavLink></li>
+              <li ><NavLink to="/about" >ABOUT</NavLink></li>
+              <li ><NavLink to="/favorites" >FAVORITES</NavLink></li>
+              <li ><NavLink to="" >RANDOM</NavLink></li>
+              <li ><NavLink to="" onClick={handleLogOut} >LOGOUT</NavLink></li>
+             </ul>
+          </div>
+          </div>
+        </nav>
         </nav>
       );
     };
+
+        
+      
 
 export default Nav;
