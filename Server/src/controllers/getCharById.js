21038cff46472307d99a102e2b186935f99d54a5
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-19 16:38:30 -0300

    19/4

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -3,13 +3,13 @@ const axios = require("axios")
 
 
 const getCharById = (res, id) => {
-    axios.get(`${URL}/characters7${id}`)
+    axios.get(`${URL}/characters/${id}`)
     .then((response) => {
         const { name, gender, species, origin, image, status } = response.data;
         res.writeHead(200, {"content-Type": "application/json"});
         res.end(JSON.stringify({ name, gender, species, origin, image, status }))
     })
-    
+
     .catch((error) => {
         res.writeHead(500, { "Content-Type": "text/plain"});
         res.end(error.message);
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,17 +1,20 @@
 const http = require('http')
-const data = require('./utils/data')
+const getCharById = require('./controllers/getCharById')
 
 http
 .createServer((request,response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
     if(request.url.includes("/rickandmorty/character")){
             const id = request.url.split("/").at(-1)
-            const character = data.find(character => character.id === +id)
-            if(character){
-                return response
-                .writeHead(200,{"Content.Type":"application/json"})
-                .end(JSON.stringify(character))
-            }
+
+            
+            // const character = data.find(character => character.id === +id)
+            // if(character){
+            //     return response
+            //     .writeHead(200,{"Content.Type":"application/json"})
+            //     .end(JSON.stringify(character))
+            // }
+            getCharById(response, id);
     }
 })
 .listen(3001)
