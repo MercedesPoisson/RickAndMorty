031902fbe79493e0e03b2c8ae55402e257246100
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-19 16:13:20 -0300

    19/4

---
+++ Server/src/controllers/getCharById.js
@@ -0,0 +1,19 @@
+const URL = (`https://rickandmortyapi.com/api`)
+const axios = require("axios")
+
+
+const getCharById = (res, id) => {
+    axios.get(`${URL}/characters7${id}`)
+    .then((response) => {
+        const { name, gender, species, origin, image, status } = response.data;
+        res.writeHead(200, {"content-Type": "application/json"});
+        res.end(JSON.stringify({ name, gender, species, origin, image, status }))
+    })
+    
+    .catch((error) => {
+        res.writeHead(500, { "Content-Type": "text/plain"});
+        res.end(error.message);
+    })    
+}
+
+module.exports = getCharById;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,22 +1,17 @@
-const http = require('http');
-const data = require("./utils/data");
+const http = require('http')
+const data = require('./utils/data')
 
-http.createServer((request, response) => {
+http
+.createServer((request,response) => {
     response.setHeader('Access-Control-Allow-Origin', '*');
-
-    const { url } = request;
-    if(url.includes("rickandmorty/character/")){
-        const id = url.split("/").at(-1);
-        const character = data.find((char) => char.id == id);
-        
-        if(character) {
-        response.writeHead(200, {"Content-Type": "application/json"});
-        response.end(JSON.stringify(character));
-    } else {
-        response.writeHead(404, {"Content-Type": "application/json"});
-        return response.end(JSON.stringify({error: "Character not found"}));
-      }
+    if(request.url.includes("/rickandmorty/character")){
+            const id = request.url.split("/").at(-1)
+            const character = data.find(character => character.id === +id)
+            if(character){
+                return response
+                .writeHead(200,{"Content.Type":"application/json"})
+                .end(JSON.stringify(character))
+            }
     }
-    
 })
-.listen(3001, "localhost")
+.listen(3001)
