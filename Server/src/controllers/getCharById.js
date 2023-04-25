83d5945158bebf42503151a282b34c070baf58de
MercedesPoisson <mercedespoisson@yahoo.com>
2023-04-25 08:45:34 -0300

    25/4

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -34,43 +34,6 @@ async function getCharById(req, res) {
     
     }
   
-
-
   module.exports = {
     getCharById
   }
-
-
-
-
-
-// const axios = require("axios")
-
-// const getCharById = (res, id) => {
-//     axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-//     .then(response => response.data)
-//     .then(({ name, gender, species, origin, image, status }) => {      
-//         const character = {
-//             id,
-//             name,
-//             gender,
-//             species,
-//             origin,
-//             image,
-//             status
-//         }   
-
-//         return res
-//                 .writeHead(200, {"content-Type": "application/json"})
-//                 .end(JSON.stringify(character))
-//     })
-//     .catch(error => {
-//         return res
-//                 .writeHead(500, { "Content-Type": "text/plain"})
-//                 .end(error.message);
-//     })    
-// }
-
-// module.exports = {
-//     getCharById
-// };
